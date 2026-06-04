c0199538c6c4ef078c029417b26c98119186ae9d
garylee920209 <7369zy@gmail.com>
2026-06-04 20:42:15 +0800

    Revert "rewrite in React"
    This reverts commit 5b1b4d6722fb16bd41c76733bbf9f8c5a95139cc.

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer(){
-  return (
-    <footer>
-      <p style={{fontSize:'small'}}>&copy; 2026 Kuan-Yi (Gary) Lee.</p>
-    </footer>
-  )
-}
